83cda08624a98090ee87bfc31ca5048820f8411a
徐栖 <haoyang@expoundite.net>
2020-05-25 17:21:13 +0800

    linking emacs

--- fsb-base/dot.sh
+++ fsb-base/dot.sh
@@ -4,7 +4,9 @@ unlink ~/.ssh
 unlink ~/.bashrc
 unlink ~/.local/bin
 unlink ~/.tmux.conf
+unlink ~/.emacs.d
 ln -s $DIR/dotfiles/ssh ~/.ssh
 ln -s $DIR/dotfiles/bash/bashrc ~/.bashrc
 ln -s $DIR/local-bin ~/.local/bin
 ln -s $DIR/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s $DIR/dotfiles/emacs ~/.emacs.d/
