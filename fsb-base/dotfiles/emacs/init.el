5e0476eae72635e8245482eb82a0f0d85248915f
徐栖 <haoyang@expoundite.net>
2020-05-25 22:17:55 +0800

    theme

--- fsb-base/dotfiles/emacs/init.el
+++ fsb-base/dotfiles/emacs/init.el
@@ -39,3 +39,23 @@
    :keymaps 'normal
    "gg" 'magit-status
    "gb" 'magit-branch))
+
+;; * Theme
+(use-package gruvbox-theme :ensure t
+  :config
+  (load-theme 'gruvbox-dark-hard))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
