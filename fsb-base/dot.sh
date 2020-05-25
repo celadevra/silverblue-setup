82de47e279be84ae2bcb0f37ff25a9b28d9c15f3
徐栖 <haoyang@expoundite.net>
2020-05-25 17:12:53 +0800

    tmux conf

--- fsb-base/dot.sh
+++ fsb-base/dot.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+unlink ~/.ssh
+unlink ~/.bashrc
+unlink ~/.local/bin
+unlink ~/.tmux.conf
 ln -s $DIR/dotfiles/ssh ~/.ssh
 ln -s $DIR/dotfiles/bash/bashrc ~/.bashrc
 ln -s $DIR/local-bin ~/.local/bin
+ln -s $DIR/dotfiles/tmux/tmux.conf ~/.tmux.conf
