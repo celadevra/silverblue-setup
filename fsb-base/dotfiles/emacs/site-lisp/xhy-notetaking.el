cf5ded1393926e89756a12c16656622a8c49924e
Haoyang Xu <haoyang@expoundite.net>
2020-06-18 15:11:00 +0800

    org mode settings

--- fsb-base/dotfiles/emacs/site-lisp/xhy-notetaking.el
+++ fsb-base/dotfiles/emacs/site-lisp/xhy-notetaking.el
@@ -2,7 +2,7 @@
 
 ;; org-mode customization
 (setq org-agenda-files '("~/Documents/org/"))
-(setq org-agenda-start-on-weekday nil)
+(setq org-agenda-start-on-weekday nil) ; show agenda for next 7 days
 (setq org-agenda-custom-commands
       '(("n" "Agenda and all TODOs"
 	 ((alltodo "" nil))
@@ -17,12 +17,23 @@
 	 "* %^{Todo|NOTE|TODAY|BACKLOG|MARK} %? %^G\nSCHEDULED: %t\n\n  Related: %a" :time-prompt t)
 	("n" "Duplicate to" entry (function xhy/find-org-capture-target)
 	 "%(xhy/get-current-heading-content)" :time-prompt t)))
+(setq org-id-link-to-org-use-id t) ; use id as link target by default
+
+(add-hook 'org-mode-hook 'auto-fill-mode)
 
 (add-hook 'org-capture-before-finalize-hook 'org-id-get-create)
 ;;(add-hook 'org-capture-after-finalize-hook 'xhy/checkin-zettel)
 (setq org-todo-keywords '((sequence "|" "NOTE")
 			  (sequence "TODAY(t!)" "|" "BACKLOG(b!)" "DONE(d!)" "CANCELLED(c!)")
 			  (sequence "|" "MARK(m!)" "ONGOING(o!)" "FINISHED(f)")))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (shell . t)
+   (js . t)
+   (ruby . t)))
+
 ;; keybindings
 (my-leader-def
   "oa"  'org-agenda
@@ -30,6 +41,34 @@
   "ol"  'org-store-link
   "oL"  'org-insert-link
   "ot"  'org-insert-structure-template)
+
+(my-leader-def
+  "or" 'xhy/zettel-search-back-link)
+
+(my-leader-def
+  "ok" 'xhy/zettel-create-link-with-search
+  "oo" 'counsel-org-goto
+  "oO" 'counsel-org-goto-all)
+
+;; remove spaces when exporting chinese texts
+(defadvice org-html-paragraph (before org-html-paragraph-advice
+				      (paragraph contents info) activate)
+  "Join consecutive Chinese lines into a single long line without
+unwanted space when exporting org-mode to html."
+  (let* ((origin-contents (ad-get-arg 1))
+	 (fix-regexp "[[:multibyte:]]")
+	 (fixed-contents
+	  (replace-regexp-in-string
+	   (concat
+	    "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
+
+    (ad-set-arg 1 fixed-contents)))
+
+;; org-ref
+(use-package org-ref :ensure t)
+
+(setq org-ref-default-bibliography '("~/Documents/org/references.bib"))
+(setq reftex-default-bibliography '("~/Documents/org/references.bib"))
 
 ;; Helper functions
 (defun xhy/find-org-capture-target ()
@@ -94,5 +133,64 @@ but only tags search returns meaningful results."
 
 ;; test
 ;; (assert (eql 102 (length (xhy/org-get-ids-with-tags "emacs|ruby"))))
+(defun xhy/zettel-search-back-link ()
+  "Search for zettels that link to this zettel, and show them in agenda view."
+  (interactive)
+  (let ((this-id (org-id-get)))
+    (if this-id
+	(org-search-view nil (concat "[id:" this-id))
+      (message "This zettel has no ID yet."))))
+
+(defun xhy/get-headings ()
+  "Get headings and ids in each org entry with TODO keywords.
+  Store them in an alist like this:
+  '((heading1 . id1)
+    (heading2 . id2)
+    ...)
+  and return the alist."
+  ()
+  ;; make file list based on org-agenda-files
+  (let ((f-list ())
+	(headings ())
+	(ids ())
+	(headings-alist))
+    (dolist (d-or-f-name org-agenda-files)
+      (if (directory-name-p d-or-f-name)
+	  (setq f-list (append (directory-files d-or-f-name t "^[0-9]\\{4\\}-[0-9]\\{2\\}\\.org$") f-list))
+	(push d-or-f-name f-list)))
+    ;; one file at a time, get every heading and their ID
+    (org-map-entries
+     (lambda () (push (org-get-heading) headings)
+       (push (org-id-get-create) ids)
+       (while (and headings ids) ; don't have zip, so a hack
+	 (push (cons (pop headings) (pop ids)) headings-alist)))
+     "TODO={.+}"
+     f-list)
+    headings-alist))
+
+(defun xhy/insert-link-from-ivy-selection (head-id-cons)
+  "Insert link to EID at current region, or if no region, insert
+  a link to EID with the description part as HEADING." 
+  (interactive)
+  (if (use-region-p)
+      (org-insert-link nil (concat "id:" (cdr head-id-cons)))
+    (org-insert-link nil (concat "id:" (cdr head-id-cons)) (car head-id-cons))))
+
+(defun xhy/zettel-create-link-with-search (&optional beg end)
+  "With a selected region, read from user input for the search
+  term. Text in the region will be used as default. Use it to
+  narrow down the candidate list. Once the user select an
+  entry, insert link at the region."
+  (interactive "r")
+  (let ((initial-input (if (use-region-p)
+			   (buffer-substring beg end)
+			 (thing-at-point 'symbol t))))
+    (ivy-read "Search string: "     ; PROMPT
+	      (xhy/get-headings)    ; COLLECTION
+	      :predicate nil
+	      :require-match t
+	      :initial-input initial-input
+	      :action 'xhy/insert-link-from-ivy-selection)))
+
 
 (provide 'xhy-notetaking)
